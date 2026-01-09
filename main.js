a94b1e38c0164b62cad74772bfb28c7b74ef7303
LOT-Dylan <lot.dyl4n@gmail.com>
2026-01-09 13:53:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const mainTitle = document.querySelector
+
+console.log(mainTitle)
+
+const cardText = document.querySelector
+
+const cards = document.querySelectorAll(".cards")
+
+function sayHello() {
+    console.log("hello")
+}
